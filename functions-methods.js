08f52be7418c2115d9277269c5210e0936ab1627
TommyBerkmortel <B_tommy_@hotmail.com>
2022-03-30 21:40:51 +0200

    opdracht 1 en 2 gemaakt

--- functions-methods.js
+++ functions-methods.js
@@ -9,8 +9,14 @@
 // getEmailDomain("t.mellink@novi.nl") geeft novi.nl
 // getEmailDomain("a.wiersma@outlook.com") geeft outlook.com
 
+/*function getEmailDomain(emailAdres) {
+    domeinNaam = emailAdres.split("@");
+    return domeinNaam[1];
+}
 
-
+console.log(getEmailDomain("n.eeken@novi-education.nl"));
+console.log(getEmailDomain("t.mellink@novi.nl"));
+console.log(getEmailDomain("a.wiersma@outlook.com"));*/
 
 /* Opdracht  2 */
 // Schrijf een functie genaamd typeOfEmail, die een emailadres verwacht. De functie checkt of het emailadres een novi domein heeft (medewerker), een novi-education domein (student), of extern domein (zoals gmail of outlook)
@@ -20,7 +26,24 @@
 // typeOfEmail("novi.nlaapjesk@outlook.com") geeft geeft "Extern" <-- deze moet het ook doen!
 // typeOfEmail("a.wiersma@outlook.com") geeft "Extern"
 
+const domainOne = typeOfEmail("n.eeken@novi-education.nl");
+const domainTwo = typeOfEmail("t.mellink@novi.nl");
+const domainThree = typeOfEmail("novi.nlaapjesk@outlook.com");
+const domainFour = typeOfEmail("a.wiersma@outlook.com");
 
+function typeOfEmail(emailAdres) {
+    splittingEmailAdres = emailAdres.split("@");
+    const domeinNaam = splittingEmailAdres[1];
+
+    if (domeinNaam === "novi-education.nl") {
+        return "Student";
+    } else if (domeinNaam === "novi.nl") {
+        return "Medewerker";
+    } else {
+        return "Extern";
+    }
+}
+console.log(domainOne, domainTwo, domainThree,domainFour);
 
 /* Opdracht  3 */
 // Schrijf een functie genaamd checkEmailValidity, die een emailadres verwacht en checkt of het emailadres valide is. De functie returned true of false, afhankelijk van de uitkomst.
@@ -34,3 +57,4 @@
 // checkEmailValidity("n.eekenanovi.nl") geeft false - want geen @
 // checkEmailValidity("n.eeken@novinl.") geeft false - want de punt mag niet als laatst
 // checkEmailValidity("tessmellink@novi,nl") geeft false - want er staat een komma in
+
